9c311db11ee94530c2d394c436f5031a50a8dc18
Nilesh Rajpurohit <46248484+mfdoomer@users.noreply.github.com>
2022-03-07 12:01:53 +0530

    Update random_movie_generator.py

--- random_movie_generator.py
+++ random_movie_generator.py
@@ -6,10 +6,18 @@ genre = [
     'Romance', 'Sci-fi', 'Thriller', 'Western'
 ]
 numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+alphabets = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
 
 random_platform = random.choice(platforms)
 random_genre = random.choice(genre)
 random_number = random.choice(numbers)
+random_alphabets = random.choice(alphabets)
+# print(
+#     f"Go to {random_platform} select {random_genre} and watch {random_number}th movie."
+# )
 print(
-    f"Go to {random_platform} select {random_genre} and watch {random_number}th movie."
+    f"Movie starting with letter {random_alphabets} in row/column {random_number} in {random_genre} genre on {random_platform}"
 )
